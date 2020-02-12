5b1c93855097b5486770419bceff4a475a3d5316
Gil <sgy@tpay.co.kr>
2020-02-12 18:54:34 +0900

    API URL 변경

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import apis
 
 urlpatterns_api_posts = [
-    path('posts/', apis.PostListCreateAPIView.as_view()),
+    path('', apis.PostListCreateAPIView.as_view()),
     path('postDetail/<int:pk>/', apis.PostDetail.as_view()),
     path('postLikeCreate/<int:post_pk>/', apis.PostLikeCreate.as_view()),
     path('commentCreate/<int:post_pk>/', apis.CommentView.as_view()),
