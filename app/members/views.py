771f84f6e79ef8fd7c177a874c7a0358358dbba0
Jeon Young Hoon <hungyb0924@gmail.com>
2020-03-18 06:29:00 +0900

    로컬 소셜로그인 - 페이스북, 카카오톡 완료 배포 서버에 붙이기- 1

--- app/members/views.py
+++ app/members/views.py
@@ -8,8 +8,8 @@ from rest_framework.response import Response
 # Create your views here.
 from rest_framework import viewsets, permissions, status
 from rest_framework.views import APIView
+from django.conf import settings
 
-from config import settings
 from config.settings.base import KAKAO_APP_ID
 from .models import Users
 from .serializers import UserProfileSerializer, ChangePasswordSerializer
@@ -66,18 +66,84 @@ class UpdatePassword(APIView):
 
 
 def facebook_login_page(request):
-    return render(request, 'login.html')
+    context = {
+        'user': request.user
+    }
+    return render(request, 'login.html', context)
 
 
 def facebook_login(request):
-    user = authenticate(request, facebook_request_token=request.GET.get('code'))
-    if user:
-        login(request, user)
-        context = {
-            'user': user,
-        }
-        return render(request, 'login.html', context)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    api_base = 'https://graph.facebook.com/v3.2'
+    api_get_access_token = f'{api_base}/oauth/access_token'
+    api_me = f'{api_base}/me'
+
+    # user = authenticate(request, facebook_request_token=request.GET.get('code'))
+    code = request.GET.get('code')
+
+    # request token을 access token으로 교환
+    params = {
+        'client_id': settings.FACEBOOK_APP_ID,
+        'redirect_uri': 'http://localhost:8000/members/facebook-login/',
+        'client_secret': settings.FACEBOOK_APP_SECRET,
+        'code': code,
+    }
+    response = requests.get(api_get_access_token, params)
+    # 인수로 전달한 문자열이 'JSON'형식일 것으로 생각
+    # json.loads는 전달한 문자열이 JSON형식일 경우, 해당 문자열을 parsing해서 파이썬 Object를 리턴함
+    # response_object = json.loads(response.text)
+    data = response.json()
+    access_token = data['access_token']
+    params = {
+        'access_token': access_token,
+        'fields': ','.join([
+            'id',
+            'first_name',
+            'last_name',
+            'picture.type(large)',
+        ])
+    }
+    response = requests.get(api_me, params)
+    data = response.json()
+
+    facebook_id = data['id']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    url_image = data['picture']['data']['url']
+    # http get 요청의 응답을 받아와서, binary data 를 img_data 에 할당
+    img_response = requests.get(url_image)
+    img_data = img_response.content
+
+    # 응답의 binary data를 사용해서, in-memory binary stream(file) 객체를 생성,
+    # f = io.ByteIO(img_response.content)
+
+    # FileField가 지원하는 InMemoryUploadedFile 객체를 사용하기,
+    # imghdr 모듈을 사용해서 페이스북에서 받은 파일 확장자를 확인
+    ext = imghdr.what('', h=img_data)
+    f = SimpleUploadedFile(f'{facebook_id}.{ext}', img_response.content)
+
+    try:
+        user = User.objects.get(username=facebook_id)
+        user.last_name = last_name
+        user.first_name = first_name
+        user.save()
+    except User.DoesNotExist:
+        user = User.objects.create_user(
+            username=facebook_id,
+            first_name=first_name,
+            last_name=last_name,
+            img_profile=f,
+        )
+    login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    return redirect('login-page')
+
+    # return user
+    #
+    # if user:
+    #     login(request, user)
+    #     context = {
+    #         'user': user,
+    #     }
+    #     return render(request, 'login.html', context)
 
 
 def kakao_login(request):
@@ -131,4 +197,5 @@ def kakao_login(request):
             img_profile=f,
         )
 
-    return HttpResponse(user)
+    login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    return redirect('login-page')
